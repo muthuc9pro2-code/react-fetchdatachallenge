e8fd42f7b48ac5b6d61b0676b84e0d667dcd34f2
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-04-27 18:21:02 +0530

    2nd challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Form from './Form';
+import List from './List';
 
 function App() {
-    
+   const API_URL = 'https://jsonplaceholder.typicode.com/';
+   const [reqType, setReqType] = useState('users');
+   const [items, setItems] = useState([]);
+
+   useEffect (() => {
+      const fetchItems = async() => {
+        try {
+            const response = await fetch(`${API_URL}${reqType}`)
+            if (! response.ok) throw Error('Something fucked up')
+            const data = await response.json();
+            console.log(data)
+            setItems(data)
+        } catch (err) {
+            console.log(err.message)
+        } 
+      }
+      fetchItems();
+   },[reqType])
+
+   return (
+    <div className='App'>
+        <Form 
+        reqType={reqType}
+        setReqType={setReqType}
+        />
+        <List items={items} />
+    </div>
+   )
 }
 
 export default App
